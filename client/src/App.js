4def1052b67606fd8cca75a24930adf61b856d2e
Angela Lim <>
2021-05-18 01:19:18 +0000

    starting changes for blog

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <div className = "container">
+        <div className = "row">
+          <div className = "col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+            <a href='/'> Home</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
